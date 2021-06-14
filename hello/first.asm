89240de5b70c0a4a4e18fbdd9192c4ef84822ac7
siavava <amittaijoel@outlook.com>
2021-06-14 12:57:31 -0400

    refactored buffer usage

--- hello/first.asm
+++ hello/first.asm
@@ -6,12 +6,12 @@
 section .text
 global _start
 _start:                     ; entrypoint
-  mov edx, len              ; get message len to edx
+  mov dx, len               ; get message len to edx
   mov ecx, msg              ; get message to exc
-  mov ebx, 1                ; fd #1       -> stdout
-  mov eax, 4                ; sys call #4 -> write to fd in ebx
+  mov bx, 1                 ; fd #1       -> stdout
+  mov ax, 4                 ; sys call #4 -> write to fd in ebx
   int 0x80                  ; kernel call -> exec $eax
-  mov eax, 1                ; sys call #1 -> exit
+  mov ax, 1                 ; sys call #1 -> exit
   int 0x80                  ; kernel call -> exec $eax
 
 section .data
